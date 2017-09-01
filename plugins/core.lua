afce8d1b2d4aad1c8ca6c1b245e47ce54d5a6ff0
Paydar <omid.paydar1818@gmail.com>
2017-09-02 04:26:52 +0430

    Update core.lua

--- plugins/core.lua
+++ plugins/core.lua
@@ -1,4 +1,4 @@
---Begin Core.lua By #BeyondTeam
+--Begin Core.lua By #paydarTeam
 local function getindex(t,id) 
 	for i,v in pairs(t) do 
 		if v == id then 
@@ -13,7 +13,7 @@ local function reload_plugins( )
 	load_plugins() 
 end
 
---By @SoLiD021
+--By @Omid_Paydar
 local function already_sudo(user_id)
 	for k,v in pairs(_config.sudo_users) do
 		if user_id == v then
@@ -24,7 +24,7 @@ local function already_sudo(user_id)
 	return false
 end
 
---By @SoLiD
+--By @Omid_Paydar
 local function sudolist(msg)
 	local sudo_users = _config.sudo_users
 	local text = "Sudo Users :\n"
@@ -49,7 +49,7 @@ local lang = redis:get(hash)
 			{text = '🔛 More ', callback_data = '/more:'..GP_id}
 		},
 		{
-			{text = '📁 About Us ', callback_data = '/beyond:'..GP_id}
+			{text = '📁 About Us ', callback_data = '/paydar:'..GP_id}
 		},
 		{
 			{text= '🔙 Back' ,callback_data = '/lang:'..GP_id}
@@ -67,7 +67,7 @@ local lang = redis:get(hash)
 			{text = '🔛 قابلیت های بیشتر', callback_data = '/more:'..GP_id}
 		},
 		{
-			{text = '📁 درباره ما ', callback_data = '/beyond:'..GP_id}
+			{text = '📁 درباره ما ', callback_data = '/paydar:'..GP_id}
 		},
 		{
 			{text= '🔙 بازگشت' ,callback_data = '/lang:'..GP_id}
@@ -86,27 +86,27 @@ local settings = data[tostring(GP_id)]["settings"]
 	keyboard = {} 
 	keyboard.inline_keyboard = {
 		{
-			{text = '🔹 Flood Sensitivity ', callback_data = 'BeyondTeam'}
+			{text = '🔹 Flood Sensitivity ', callback_data = 'paydarTeam'}
 		},
 		{
 			{text = "➕", callback_data='/floodup:'..GP_id}, 
-			{text = tostring(settings.num_msg_max), callback_data="BeyondTeam"},
+			{text = tostring(settings.num_msg_max), callback_data="paydarTeam"},
 			{text = "➖", callback_data='/flooddown:'..GP_id}
 		},
 		{
-			{text = '🔹 Character Sensitivity ', callback_data = 'BeyondTeam'}
+			{text = '🔹 Character Sensitivity ', callback_data = 'paydarTeam'}
 		},
 		{
 			{text = "➕", callback_data='/charup:'..GP_id}, 
-			{text = tostring(settings.set_char), callback_data="BeyondTeam"},
+			{text = tostring(settings.set_char), callback_data="paydarTeam"},
 			{text = "➖", callback_data='/chardown:'..GP_id}
 		},
 		{
-			{text = '🔹 Flood Check Time ', callback_data = 'BeyondTeam'}
+			{text = '🔹 Flood Check Time ', callback_data = 'paydarTeam'}
 		},
 		{
 			{text = "➕", callback_data='/floodtimeup:'..GP_id}, 
-			{text = tostring(settings.time_check), callback_data="BeyondTeam"},
+			{text = tostring(settings.time_check), callback_data="paydarTeam"},
 			{text = "➖", callback_data='/floodtimedown:'..GP_id}
 		},
 		{
@@ -118,27 +118,27 @@ local settings = data[tostring(GP_id)]["settings"]
 	keyboard = {} 
 	keyboard.inline_keyboard = {
 		{
-			{text = '🔹 حداکثر پیام های مکرر ', callback_data = 'BeyondTeam'}
+			{text = '🔹 حداکثر پیام های مکرر ', callback_data = 'paydarTeam'}
 		},
 		{
 			{text = "➕", callback_data='/floodup:'..GP_id}, 
-			{text = tostring(settings.num_msg_max), callback_data="BeyondTeam"},
+			{text = tostring(settings.num_msg_max), callback_data="paydarTeam"},
 			{text = "➖", callback_data='/flooddown:'..GP_id}
 		},
 		{
-			{text = '🔹 حداکثر حروف مجاز ', callback_data = 'BeyondTeam'}
+			{text = '🔹 حداکثر حروف مجاز ', callback_data = 'paydarTeam'}
 		},
 		{
 			{text = "➕", callback_data='/charup:'..GP_id}, 
-			{text = tostring(settings.set_char), callback_data="BeyondTeam"},
+			{text = tostring(settings.set_char), callback_data="paydarTeam"},
 			{text = "➖", callback_data='/chardown:'..GP_id}
 		},
 		{
-			{text = '🔹 زمان بررسی پیام های مکرر ', callback_data = 'BeyondTeam'}
+			{text = '🔹 زمان بررسی پیام های مکرر ', callback_data = 'paydarTeam'}
 		},
 		{
 			{text = "➕", callback_data='/floodtimeup:'..GP_id}, 
-			{text = tostring(settings.time_check), callback_data="BeyondTeam"},
+			{text = tostring(settings.time_check), callback_data="paydarTeam"},
 			{text = "➖", callback_data='/floodtimedown:'..GP_id}
 		},
 		{
@@ -227,55 +227,55 @@ local lang = redis:get(hash)
 	keyboard = {} 
 	keyboard.inline_keyboard = {
 		{
-			{text = "🔹 Lock Edit", callback_data='BeyondTeam'}, 
+			{text = "🔹 Lock Edit", callback_data='paydarTeam'}, 
 			{text = lock_edit, callback_data="/lockedit:"..GP_id}
 		},
 		{
-			{text = "🔹 Lock Link", callback_data='BeyondTeam'}, 
+			{text = "🔹 Lock Link", callback_data='paydarTeam'}, 
 			{text = lock_link, callback_data="/locklink:"..GP_id}
 		},
 		{
-			{text = "🔹 Lock Tags", callback_data='BeyondTeam'}, 
+			{text = "🔹 Lock Tags", callback_data='paydarTeam'}, 
 			{text = lock_tag, callback_data="/locktags:"..GP_id}
 		},
 		{
-			{text = "🔹 Lock Join", callback_data='BeyondTeam'}, 
+			{text = "🔹 Lock Join", callback_data='paydarTeam'}, 
 			{text = lock_join, callback_data="/lockjoin:"..GP_id}
 		},
 		{
-			{text = "🔹 Lock Flood", callback_data='BeyondTeam'}, 
+			{text = "🔹 Lock Flood", callback_data='paydarTeam'}, 
 			{text = lock_flood, callback_data="/lockflood:"..GP_id}
 		},
 		{
-			{text = "🔹 Lock Spam", callback_data='BeyondTeam'}, 
+			{text = "🔹 Lock Spam", callback_data='paydarTeam'}, 
 			{text = lock_spam, callback_data="/lockspam:"..GP_id}
 		},
 		{
-			{text = "🔹 Lock Mention", callback_data='BeyondTeam'}, 
+			{text = "🔹 Lock Mention", callback_data='paydarTeam'}, 
 			{text = lock_mention, callback_data="/lockmention:"..GP_id}
 		},
 		{
-			{text = "🔹 Lock Arabic", callback_data='BeyondTeam'}, 
+			{text = "🔹 Lock Arabic", callback_data='paydarTeam'}, 
 			{text = lock_arabic, callback_data="/lockarabic:"..GP_id}
 		},
 		{
-			{text = "🔹 Lock Webpage", callback_data='BeyondTeam'}, 
+			{text = "🔹 Lock Webpage", callback_data='paydarTeam'}, 
 			{text = lock_webpage, callback_data="/lockwebpage:"..GP_id}
 		},
 		{
-			{text = "🔹 Lock Markdown", callback_data='BeyondTeam'}, 
+			{text = "🔹 Lock Markdown", callback_data='paydarTeam'}, 
 			{text = lock_markdown, callback_data="/lockmarkdown:"..GP_id}
 		},
 		{
-			{text = "🔹 Lock Pin", callback_data='BeyondTeam'}, 
+			{text = "🔹 Lock Pin", callback_data='paydarTeam'}, 
 			{text = lock_pin, callback_data="/lockpin:"..GP_id}
 		},
 		{
-			{text = "🔹 Lock Bots", callback_data='BeyondTeam'}, 
+			{text = "🔹 Lock Bots", callback_data='paydarTeam'}, 
 			{text = lock_bots, callback_data="/lockbots:"..GP_id}
 		},
 		{
-			{text = "🔹 Group Welcome", callback_data='BeyondTeam'}, 
+			{text = "🔹 Group Welcome", callback_data='paydarTeam'}, 
 			{text = group_welcone, callback_data="/welcome:"..GP_id}
 		},
 		{
@@ -290,55 +290,55 @@ local lang = redis:get(hash)
 	keyboard = {} 
 	keyboard.inline_keyboard = {
 		{
-			{text = "🔹 قفل ویرایش", callback_data='BeyondTeam'}, 
+			{text = "🔹 قفل ویرایش", callback_data='paydarTeam'}, 
 			{text = lock_edit, callback_data="/lockedit:"..GP_id}
 		},
 		{
-			{text = "🔹 قفل لینک", callback_data='BeyondTeam'}, 
+			{text = "🔹 قفل لینک", callback_data='paydarTeam'}, 
 			{text = lock_link, callback_data="/locklink:"..GP_id}
 		},
 		{
-			{text = "🔹 قفل تگ", callback_data='BeyondTeam'}, 
+			{text = "🔹 قفل تگ", callback_data='paydarTeam'}, 
 			{text = lock_tag, callback_data="/locktags:"..GP_id}
 		},
 		{
-			{text = "🔹 قفل ورود", callback_data='BeyondTeam'}, 
+			{text = "🔹 قفل ورود", callback_data='paydarTeam'}, 
 			{text = lock_join, callback_data="/lockjoin:"..GP_id}
 		},
 		{
-			{text = "🔹 قفل پیام های مکرر", callback_data='BeyondTeam'}, 
+			{text = "🔹 قفل پیام های مکرر", callback_data='paydarTeam'}, 
 			{text = lock_flood, callback_data="/lockflood:"..GP_id}
 		},
 		{
-			{text = "🔹 قفل هرزنامه", callback_data='BeyondTeam'}, 
+			{text = "🔹 قفل هرزنامه", callback_data='paydarTeam'}, 
 			{text = lock_spam, callback_data="/lockspam:"..GP_id}
 		},
 		{
-			{text = "🔹 قفل فراخوانی", callback_data='BeyondTeam'}, 
+			{text = "🔹 قفل فراخوانی", callback_data='paydarTeam'}, 
 			{text = lock_mention, callback_data="/lockmention:"..GP_id}
 		},
 		{
-			{text = "🔹 قفل عربی", callback_data='BeyondTeam'}, 
+			{text = "🔹 قفل عربی", callback_data='paydarTeam'}, 
 			{text = lock_arabic, callback_data="/lockarabic:"..GP_id}
 		},
 		{
-			{text = "🔹 قفل صفحات وب", callback_data='BeyondTeam'}, 
+			{text = "🔹 قفل صفحات وب", callback_data='paydarTeam'}, 
 			{text = lock_webpage, callback_data="/lockwebpage:"..GP_id}
 		},
 		{
-			{text = "🔹 قفل فونت", callback_data='BeyondTeam'}, 
+			{text = "🔹 قفل فونت", callback_data='paydarTeam'}, 
 			{text = lock_markdown, callback_data="/lockmarkdown:"..GP_id}
 		},
 		{
-			{text = "🔹 قفل سنجاق کردن", callback_data='BeyondTeam'}, 
+			{text = "🔹 قفل سنجاق کردن", callback_data='paydarTeam'}, 
 			{text = lock_pin, callback_data="/lockpin:"..GP_id}
 		},
 		{
-			{text = "🔹 قفل ربات ها", callback_data='BeyondTeam'}, 
+			{text = "🔹 قفل ربات ها", callback_data='paydarTeam'}, 
 			{text = lock_bots, callback_data="/lockbots:"..GP_id}
 		},
 		{
-			{text = "🔹 خوشآمد گویی", callback_data='BeyondTeam'}, 
+			{text = "🔹 خوشآمد گویی", callback_data='paydarTeam'}, 
 			{text = group_welcone, callback_data="/welcome:"..GP_id}
 		},
 		{
@@ -445,67 +445,67 @@ local lang = redis:get(hash)
 	keyboard = {} 
 	keyboard.inline_keyboard = {
 		{
-			{text = "🔹 Mute All", callback_data='BeyondTeam'}, 
+			{text = "🔹 Mute All", callback_data='paydarTeam'}, 
 			{text = mute_all, callback_data="/muteall:"..GP_id}
 		},
 		{
-			{text = "🔹 Mute Gifs", callback_data='BeyondTeam'}, 
+			{text = "🔹 Mute Gifs", callback_data='paydarTeam'}, 
 			{text = mute_gif, callback_data="/mutegif:"..GP_id}
 		},
 		{
-			{text = "🔹 Mute Text", callback_data='BeyondTeam'}, 
+			{text = "🔹 Mute Text", callback_data='paydarTeam'}, 
 			{text = mute_text, callback_data="/mutetext:"..GP_id}
 		},
 		{
-			{text = "🔹 Mute Inline", callback_data='BeyondTeam'}, 
+			{text = "🔹 Mute Inline", callback_data='paydarTeam'}, 
 			{text = mute_inline, callback_data="/muteinline:"..GP_id}
 		},
 		{
-			{text = "🔹 Mute Game", callback_data='BeyondTeam'}, 
+			{text = "🔹 Mute Game", callback_data='paydarTeam'}, 
 			{text = mute_game, callback_data="/mutegame:"..GP_id}
 		},
 		{
-			{text = "🔹 Mute Photo", callback_data='BeyondTeam'}, 
+			{text = "🔹 Mute Photo", callback_data='paydarTeam'}, 
 			{text = mute_photo, callback_data="/mutephoto:"..GP_id}
 		},
 		{
-			{text = "🔹 Mute Video", callback_data='BeyondTeam'}, 
+			{text = "🔹 Mute Video", callback_data='paydarTeam'}, 
 			{text = mute_video, callback_data="/mutevideo:"..GP_id}
 		},
 		{
-			{text = "🔹 Mute Audio", callback_data='BeyondTeam'}, 
+			{text = "🔹 Mute Audio", callback_data='paydarTeam'}, 
 			{text = mute_audio, callback_data="/muteaudio:"..GP_id}
 		},
 		{
-			{text = "🔹 Mute Voice", callback_data='BeyondTeam'}, 
+			{text = "🔹 Mute Voice", callback_data='paydarTeam'}, 
 			{text = mute_voice, callback_data="/mutevoice:"..GP_id}
 		},
 		{
-			{text = "🔹 Mute Sticker", callback_data='BeyondTeam'}, 
+			{text = "🔹 Mute Sticker", callback_data='paydarTeam'}, 
 			{text = mute_sticker, callback_data="/mutesticker:"..GP_id}
 		},
 		{
-			{text = "🔹 Mute Contact", callback_data='BeyondTeam'}, 
+			{text = "🔹 Mute Contact", callback_data='paydarTeam'}, 
 			{text = mute_contact, callback_data="/mutecontact:"..GP_id}
 		},
 		{
-			{text = "🔹 Mute Forward", callback_data='BeyondTeam'}, 
+			{text = "🔹 Mute Forward", callback_data='paydarTeam'}, 
 			{text = mute_forward, callback_data="/muteforward:"..GP_id}
 		},
 		{
-			{text = "🔹 Mute Location", callback_data='BeyondTeam'}, 
+			{text = "🔹 Mute Location", callback_data='paydarTeam'}, 
 			{text = mute_location, callback_data="/mutelocation:"..GP_id}
 		},
 		{
-			{text = "🔹 Mute Document", callback_data='BeyondTeam'}, 
+			{text = "🔹 Mute Document", callback_data='paydarTeam'}, 
 			{text = mute_document, callback_data="/mutedocument:"..GP_id}
 		},
 		{
-			{text = "🔹 Mute TgService", callback_data='BeyondTeam'}, 
+			{text = "🔹 Mute TgService", callback_data='paydarTeam'}, 
 			{text = mute_tgservice, callback_data="/mutetgservice:"..GP_id}
 		},
 		{
-			{text = "🔹 Mute Keyboard", callback_data='BeyondTeam'}, 
+			{text = "🔹 Mute Keyboard", callback_data='paydarTeam'}, 
 			{text = mute_keyboard, callback_data="/mutekeyboard:"..GP_id}
 		},
 		{
@@ -517,67 +517,67 @@ local lang = redis:get(hash)
 	keyboard = {} 
 	keyboard.inline_keyboard = {
 		{
-			{text = "🔹 بیصدا همه", callback_data='BeyondTeam'}, 
+			{text = "🔹 بیصدا همه", callback_data='paydarTeam'}, 
 			{text = mute_all, callback_data="/muteall:"..GP_id}
 		},
 		{
-			{text = "🔹 بیصدا تصاویر متحرک", callback_data='BeyondTeam'}, 
+			{text = "🔹 بیصدا تصاویر متحرک", callback_data='paydarTeam'}, 
 			{text = mute_gif, callback_data="/mutegif:"..GP_id}
 		},
 		{
-			{text = "🔹 بیصدا متن", callback_data='BeyondTeam'}, 
+			{text = "🔹 بیصدا متن", callback_data='paydarTeam'}, 
 			{text = mute_text, callback_data="/mutetext:"..GP_id}
 		},
 		{
-			{text = "🔹 بیصدا اینلاین", callback_data='BeyondTeam'}, 
+			{text = "🔹 بیصدا اینلاین", callback_data='paydarTeam'}, 
 			{text = mute_inline, callback_data="/muteinline:"..GP_id}
 		},
 		{
-			{text = "🔹 بیصدا بازی", callback_data='BeyondTeam'}, 
+			{text = "🔹 بیصدا بازی", callback_data='paydarTeam'}, 
 			{text = mute_game, callback_data="/mutegame:"..GP_id}
 		},
 		{
-			{text = "🔹 بیصدا عکس", callback_data='BeyondTeam'}, 
+			{text = "🔹 بیصدا عکس", callback_data='paydarTeam'}, 
 			{text = mute_photo, callback_data="/mutephoto:"..GP_id}
 		},
 		{
-			{text = "🔹 بیصدا فیلم", callback_data='BeyondTeam'}, 
+			{text = "🔹 بیصدا فیلم", callback_data='paydarTeam'}, 
 			{text = mute_video, callback_data="/mutevideo:"..GP_id}
 		},
 		{
-			{text = "🔹 بیصدا آهنگ", callback_data='BeyondTeam'}, 
+			{text = "🔹 بیصدا آهنگ", callback_data='paydarTeam'}, 
 			{text = mute_audio, callback_data="/muteaudio:"..GP_id}
 		},
 		{
-			{text = "🔹 بیصدا صدا", callback_data='BeyondTeam'}, 
+			{text = "🔹 بیصدا صدا", callback_data='paydarTeam'}, 
 			{text = mute_voice, callback_data="/mutevoice:"..GP_id}
 		},
 		{
-			{text = "🔹 بیصدا استیکر", callback_data='BeyondTeam'}, 
+			{text = "🔹 بیصدا استیکر", callback_data='paydarTeam'}, 
 			{text = mute_sticker, callback_data="/mutesticker:"..GP_id}
 		},
 		{
-			{text = "🔹 بیصدا مخاطب", callback_data='BeyondTeam'}, 
+			{text = "🔹 بیصدا مخاطب", callback_data='paydarTeam'}, 
 			{text = mute_contact, callback_data="/mutecontact:"..GP_id}
 		},
 		{
-			{text = "🔹 بیصدا نقل و قول", callback_data='BeyondTeam'}, 
+			{text = "🔹 بیصدا نقل و قول", callback_data='paydarTeam'}, 
 			{text = mutes.mute_forward, callback_data="/muteforward:"..GP_id}
 		},
 		{
-			{text = "🔹 بیصدا موقعیت", callback_data='BeyondTeam'}, 
+			{text = "🔹 بیصدا موقعیت", callback_data='paydarTeam'}, 
 			{text = mute_location, callback_data="/mutelocation:"..GP_id}
 		},
 		{
-			{text = "🔹 بیصدا فایل", callback_data='BeyondTeam'}, 
+			{text = "🔹 بیصدا فایل", callback_data='paydarTeam'}, 
 			{text = mute_document, callback_data="/mutedocument:"..GP_id}
 		},
 		{
-			{text = "🔹 بیصدا خدمات تلگرام", callback_data='BeyondTeam'}, 
+			{text = "🔹 بیصدا خدمات تلگرام", callback_data='paydarTeam'}, 
 			{text = mute_tgservice, callback_data="/mutetgservice:"..GP_id}
 		},
 		{
-			{text = "🔹 بیصدا کیبورد", callback_data='BeyondTeam'}, 
+			{text = "🔹 بیصدا کیبورد", callback_data='paydarTeam'}, 
 			{text = mute_keyboard, callback_data="/mutekeyboard:"..GP_id}
 		},
 		{
@@ -1309,7 +1309,7 @@ if matches[1] == '/floodtimedown' then
 				check_time = data[tostring(matches[2])]['settings']['time_check']
 			end
 		end
-		if tonumber(check_time) 🔹 2 then
+		if tonumber(check_time) > 2 then
 			check_time = tonumber(check_time) - 1
 			data[tostring(matches[2])]['settings']['time_check'] = check_time
 			save_data(_config.moderation.data, data)
@@ -2650,7 +2650,7 @@ local keyboard = {}
 end
 end
          -- ####################### About Us ####################### --
-if matches[1] == '/beyond' then
+if matches[1] == '/paydar' then
 	local text = _config.info_text
     if not lang then
 	keyboard = {} 
@@ -2701,14 +2701,14 @@ _》_*Lua*, *Cli* `and` *Api* _Bots_
 		keyboard = {} 
 		keyboard.inline_keyboard = {
 			{
-				{text = "🔙 Back", callback_data="/beyond:"..matches[2]}
+				{text = "🔙 Back", callback_data="/paydar:"..matches[2]}
 			}
 		}
   elseif lang then
 		keyboard = {} 
 		keyboard.inline_keyboard = {
 			{
-				{text = "🔙 بازگشت", callback_data="/beyond:"..matches[2]}
+				{text = "🔙 بازگشت", callback_data="/paydar:"..matches[2]}
 			}
 		}
   end
@@ -2731,14 +2731,14 @@ _》PvResan :_ [Paydar Pv](Telegram.Me/Paydar_projectbot)
 		keyboard = {} 
 		keyboard.inline_keyboard = {
 			{
-				{text = "🔙 Back", callback_data="/beyond:"..matches[2]}
+				{text = "🔙 Back", callback_data="/paydar:"..matches[2]}
 			}
 		}
   elseif lang then
 		keyboard = {} 
 		keyboard.inline_keyboard = {
 			{
-				{text = "🔙 بازگشت", callback_data="/beyond:"..matches[2]}
+				{text = "🔙 بازگشت", callback_data="/paydar:"..matches[2]}
 			}
 		}
   end
@@ -2829,7 +2829,7 @@ return {
 		"^###cb:(/modlist):(.*)$",
 		"^###cb:(/cleanmods):(.*)$",
 		"^###cb:(/bans):(.*)$",
-		"^###cb:(/beyond):(.*)$",
+		"^###cb:(/paydar):(.*)$",
 		"^###cb:(/cleanbans):(.*)$",
 		"^###cb:(/filterlist):(.*)$",
 		"^###cb:(/cleanfilterlist):(.*)$",
